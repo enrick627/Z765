80a8bf1ec1e3f8cded1ce5bc110cea143b77c756
enrick627 <enrick.demunter@student.romerocollege.be>
2020-05-06 22:27:45 +0200

    v1.2 is ook af

---
+++ Z7/OvaalZwembad.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Z7
+{
+    public class OvaalZwembad
+    {
+        /// <summary>
+        /// het totale volume van het zwembad
+        /// </summary>
+        public ushort Watervolume
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// de grote van de lange as
+        /// </summary>
+        public byte LangeAs
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// de grote van de korte as
+        /// </summary>
+        public byte KorteAs
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// de diepte van het zwembad
+        /// </summary>
+        public byte diepte
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// de tijd die het zwembad nodig heeft om te vullen
+        /// </summary>
+        public strin Vulzwembad()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
